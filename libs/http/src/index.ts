951e4cfbb32f526c7295b6dce250c89078765d1a
IKatsuba <katsuba.igor@gmail.com>
2020-12-30 14:04:38 +0300

    fix(http): fix types

--- libs/http/src/index.ts
+++ libs/http/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/http';
 export * from './lib/server/server';
 export * from './lib/server/create-server';
+
+export { ServerOptions, IncomingMessage, RequestOptions } from 'http';
--- libs/http/src/lib/http.ts
+++ libs/http/src/lib/http.ts
@@ -1,18 +1,38 @@
 import { watchify } from '@rxnode/core';
-import { get as originalGet, request as originalRequest, IncomingMessage, RequestOptions } from 'http';
+import {
+  get as originalGet,
+  IncomingMessage,
+  request as originalRequest,
+  RequestOptions,
+} from 'http';
 import { take } from 'rxjs/operators';
 import { URL } from 'url';
+import { Observable } from 'rxjs';
 
+export function get(
+  options: RequestOptions | string | URL
+): Observable<IncomingMessage>;
+export function get(
+  url: string | URL,
+  options: RequestOptions
+): Observable<IncomingMessage>;
 export function get(
   url: string | URL | RequestOptions,
   options?: RequestOptions
-) {
+): Observable<IncomingMessage> {
   return watchify<
     [url: string | URL | RequestOptions, options?: RequestOptions],
     [IncomingMessage]
   >(originalGet)(url, options).pipe(take(1));
 }
 
+export function request(
+  options: RequestOptions | string | URL
+): Observable<IncomingMessage>;
+export function request(
+  url: string | URL,
+  options: RequestOptions
+): Observable<IncomingMessage>;
 export function request(
   url: string | URL | RequestOptions,
   options?: RequestOptions
